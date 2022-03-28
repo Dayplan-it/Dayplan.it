a440d23344955a659ba8b1f44c5e8102d5809196
teahun <98328569+artdumb@users.noreply.github.com>
2022-03-28 21:38:55 +0900

    user-Model
    장고의 USER추상클래스를 활용하여 유저 모델설계

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+# 현재 별명, 전화번호 정도 필드 추가 / 이후에 필요한 거 있으면 추가할 예정
 class User(AbstractUser):
-
-    """ Custom User Model """
-
-    pass
+    nickname = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50, null=True)
