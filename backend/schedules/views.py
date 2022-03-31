d449afc4e8da935c3fbecf57272374b51d0abd7d
custardcream@kakao.com <custardcream@kakao.com>
2022-04-01 05:24:29 +0900

    Temporary Commit

--- backend/schedules/views.py
+++ backend/schedules/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from . import models as schedule_models
+from .serializers import ScheduleSerializer
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def get_schedule(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
