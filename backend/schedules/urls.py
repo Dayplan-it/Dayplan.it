cd9b672785db20eced28388e57e90e483f98b3ca
custardcream@kakao.com <custardcream@kakao.com>
2022-04-05 15:05:40 +0900

    Add Delete Schedule API

--- backend/schedules/urls.py
+++ backend/schedules/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path, include
-from .views import FindScheduleAPIView
+from django.urls import path
+from .views import FindScheduleAPIView, DeleteScheduleAPIView
 urlpatterns = [
 
     path('find', FindScheduleAPIView.as_view()),  # user_id, date param 필요
+    # path('create', .as_view()),
+    # path('update', .as_view()),
+    path('delete', DeleteScheduleAPIView.as_view()),
 
 ]
--- backend/schedules/views.py
+++ backend/schedules/views.py
@@ -1,14 +1,24 @@
+import logging
 from datetime import datetime
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK, HTTP_404_NOT_FOUND
 from .api.find_schedule import find_schedule
+from schedules import models as schedule_models
+from schedules.api.serializers import ScheduleSerializer
+
 
 # Define Param Names
 PARAM_USER_ID = 'user_id'
 PARAM_DATE = 'date'
+
+# Define Boby Keys
+KEY_USER_ID = 'user_id'
+KEY_DATE = 'date'
+
+logger = logging.getLogger('django.server')
 
 
 class FindScheduleAPIView(APIView):
@@ -33,3 +43,41 @@ class FindScheduleAPIView(APIView):
             return JsonResponse({"message": f"INVALID_PARAMETER, you must pass {PARAM_USER_ID}(Integer) and {PARAM_DATE}(Integer)"}, status=HTTP_400_BAD_REQUEST)
         except ValueError:
             return JsonResponse({"message": "INVALID_VALUE"}, status=HTTP_400_BAD_REQUEST)
+
+
+class DeleteScheduleAPIView(APIView):
+    """
+    스케쥴을 삭제하는 API,
+    user_id와 schedule의 date(Timestamp)를 body를 통해 받음
+    """
+
+    def get_object(self, user_id, date):
+        try:
+            return schedule_models.Schedule.objects.get(user_id=user_id, date=date)
+        except schedule_models.Schedule.DoesNotExist:
+            raise ObjectDoesNotExist
+
+    def delete(self, request):
+        try:
+            user_id = request.data[KEY_USER_ID]
+            date = datetime.fromtimestamp(
+                int(request.data[KEY_DATE])).date()
+
+            schedule = self.get_object(user_id, date)
+            title = schedule.schedule_title
+            username = schedule.user.username
+
+            logger.info(
+                f'{username}의 {date} 스케쥴 "{title}" 삭제')
+            schedule.delete()
+
+            return JsonResponse({"message": f"{username}의 {date} 스케쥴 \"{title}\" 삭제"}, status=HTTP_200_OK)
+
+        except ValidationError:
+            return JsonResponse({"message": "TYPE_ERROR"}, status=HTTP_400_BAD_REQUEST)
+        except ValueError:
+            return JsonResponse({"message": "INVALID_VALUE"}, status=HTTP_400_BAD_REQUEST)
+        except ObjectDoesNotExist:
+            logger.error(
+                f'{user_id}의 {date} 스케쥴이 없습니다.')
+            return JsonResponse({"message": "NO_SUCH_SCHEDULE"}, status=HTTP_404_NOT_FOUND)
