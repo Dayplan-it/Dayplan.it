32b4bf9b2a1467669bd4119ad4eaa3d592bb808b
custardcream@kakao.com <custardcream@kakao.com>
2022-04-13 23:39:53 +0900

    Update constants.dart

--- frontend/lib/constants.dart
+++ frontend/lib/constants.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 // 앱에서 사용할 색상
-const primaryColor = Color.fromARGB(255, 1, 87, 141);
-const pointColor = Color.fromARGB(255, 210, 55, 55);
-const subTextColor = Color.fromARGB(255, 175, 169, 169);
+const primaryColor = Color.fromARGB(255, 1, 87, 141); // Dayplan.it 파랑
+const pointColor = Color.fromARGB(255, 210, 55, 55); // 포인트컬러
+const subTextColor = Color.fromARGB(255, 175, 169, 169); // 회색조 컬러
 const defaultTextColor = Colors.black;
 const backgroundColor = Colors.white;
-const DayplanitLogoFont = GoogleFonts.poppins;
+const DayplanitLogoFont = GoogleFonts.poppins; // 로고용 폰트
+const mainFont = GoogleFonts.notoSans;
 
 const double dafaultPadding = 20.0;
