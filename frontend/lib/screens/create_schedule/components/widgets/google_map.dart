186dc03a2c0e042b9b73b5ea04c2612ca501c7f0
custardcream@kakao.com <custardcream@kakao.com>
2022-05-16 11:09:55 +0900

    Fix Small Bugs

--- frontend/lib/screens/create_schedule/components/widgets/google_map.dart
+++ frontend/lib/screens/create_schedule/components/widgets/google_map.dart
@@ -39,11 +39,6 @@ class _MapWithCustomInfoWindowState extends State<MapWithCustomInfoWindow> {
         },
         onLongPress: (position) async {},
         myLocationEnabled: true,
-        gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{
-          Factory<OneSequenceGestureRecognizer>(
-            () => EagerGestureRecognizer(),
-          ),
-        },
         mapToolbarEnabled: false,
         rotateGesturesEnabled: false,
         tiltGesturesEnabled: false,
@@ -69,95 +64,6 @@ Future<Marker> markerWithCustomInfoWindow(
     int? length,
     {bool isRecommended = false,
     required bool isForDecidingPlace}) async {
-  // Widget _widget() {
-  //   return FittedBox(
-  //     child: Column(
-  //       mainAxisAlignment: MainAxisAlignment.center,
-  //       children: [
-  //         Flex(
-  //             crossAxisAlignment: CrossAxisAlignment.center,
-  //             direction: Axis.horizontal,
-  //             children: [
-  //               Container(
-  //                 alignment: Alignment.center,
-  //                 padding: const EdgeInsets.fromLTRB(8, 5, 8, 5),
-  //                 decoration: BoxDecoration(
-  //                     borderRadius: defaultBoxRadius,
-  //                     boxShadow: defaultBoxShadow,
-  //                     border: Border.all(color: primaryColor, width: 2),
-  //                     color: Colors.white),
-  //                 child: Row(
-  //                   mainAxisAlignment: MainAxisAlignment.center,
-  //                   children: [
-  //                     Text(
-  //                       title,
-  //                       style: mainFont(
-  //                           color: primaryColor, fontWeight: FontWeight.w700),
-  //                     ),
-  //                     if (length != null) ...[
-  //                       const SizedBox(
-  //                         width: 5,
-  //                       ),
-  //                       Container(
-  //                         decoration: BoxDecoration(
-  //                             borderRadius:
-  //                                 BorderRadius.circular(double.infinity),
-  //                             color: Colors.white),
-  //                         child: Text(
-  //                           length < 1000
-  //                               ? length.toString() + "m"
-  //                               : (length.toDouble() / 1000)
-  //                                       .toStringAsFixed(1) +
-  //                                   "km",
-  //                           style: mainFont(
-  //                               color: subTextColor,
-  //                               fontWeight: FontWeight.w700),
-  //                         ),
-  //                       ),
-  //                     ],
-  //                     if (rating != null) ...[
-  //                       const SizedBox(
-  //                         width: 5,
-  //                       ),
-  //                       Container(
-  //                         decoration: BoxDecoration(
-  //                             borderRadius:
-  //                                 BorderRadius.circular(double.infinity),
-  //                             color: Colors.white),
-  //                         child: Row(
-  //                           crossAxisAlignment: CrossAxisAlignment.center,
-  //                           children: [
-  //                             const Icon(
-  //                               Icons.star,
-  //                               color: pointColor,
-  //                               size: 12,
-  //                             ),
-  //                             const SizedBox(
-  //                               width: 2,
-  //                             ),
-  //                             Text(
-  //                               rating.toString(),
-  //                               style: mainFont(
-  //                                   color: pointColor,
-  //                                   fontWeight: FontWeight.w700),
-  //                             ),
-  //                           ],
-  //                         ),
-  //                       ),
-  //                     ]
-  //                   ],
-  //                 ),
-  //               ),
-  //             ]),
-  //         CustomPaint(
-  //           size: const Size(20, 15),
-  //           painter: DrawTriangleShape(),
-  //         )
-  //       ],
-  //     ),
-  //   );
-  // }
-
   _onTap() {
     return showDialog(
         context: parentKey.currentContext!,
@@ -192,7 +98,6 @@ Future<Marker> markerWithCustomInfoWindow(
         title: title,
         snippet:
             "${rating != null ? "$rating점 " : ""}${length != null ? length < 1000 ? length.toString() + "m" : (length.toDouble() / 1000).toStringAsFixed(1) + "km" : ""}"),
-    onTap: _onTap,
   );
 }
 
@@ -203,7 +108,7 @@ Future<Marker> markerForCenterTarget({
   Widget _widget() {
     return const Icon(
       Icons.location_searching_rounded,
-      size: 30,
+      size: 10,
       color: primaryColor,
     );
   }
@@ -222,131 +127,6 @@ Future<Marker> markerForPlace(
     required GlobalKey parentKey,
     bool isOtherPlace = false}) async {
   MarkerId markerId = MarkerId(place.placeId!);
-  // Widget _widget() {
-  //   if (!isOtherPlace) {
-  //     Color placeColor = placeColorByPlaceType(place.placeType);
-  //     return FittedBox(
-  //       child: Column(
-  //         mainAxisAlignment: MainAxisAlignment.center,
-  //         children: [
-  //           Flex(
-  //               crossAxisAlignment: CrossAxisAlignment.center,
-  //               direction: Axis.horizontal,
-  //               children: [
-  //                 Container(
-  //                   alignment: Alignment.center,
-  //                   padding: const EdgeInsets.all(10),
-  //                   decoration: BoxDecoration(
-  //                       borderRadius: defaultBoxRadius,
-  //                       boxShadow: defaultBoxShadow,
-  //                       color: placeColor),
-  //                   child: Column(
-  //                     children: [
-  //                       Text(
-  //                         place.placeName!,
-  //                         style: mainFont(
-  //                             color: Colors.white,
-  //                             fontWeight: FontWeight.w700,
-  //                             fontSize: 14),
-  //                       ),
-  //                       const SizedBox(
-  //                         height: 8,
-  //                       ),
-  //                       Text(
-  //                         "${printDateTimeHourAndMinuteOnly(place.startsAt!)} ~ ${printDateTimeHourAndMinuteOnly(place.endsAt!)}",
-  //                         style: mainFont(
-  //                             fontWeight: FontWeight.w500,
-  //                             color: Colors.white,
-  //                             fontSize: 12,
-  //                             letterSpacing: 1),
-  //                       ),
-  //                       Text(
-  //                         printDurationKor(place.duration),
-  //                         style: mainFont(
-  //                             fontWeight: FontWeight.w500,
-  //                             color: Colors.white,
-  //                             fontSize: 12,
-  //                             letterSpacing: 1),
-  //                       ),
-  //                     ],
-  //                   ),
-  //                 ),
-  //               ]),
-  //           CustomPaint(
-  //             size: const Size(20, 15),
-  //             painter: DrawTriangleShape(fillColor: placeColor),
-  //           )
-  //         ],
-  //       ),
-  //     );
-  //   } else {
-  //     return FittedBox(
-  //       child: Column(
-  //         mainAxisAlignment: MainAxisAlignment.center,
-  //         children: [
-  //           Flex(
-  //               crossAxisAlignment: CrossAxisAlignment.center,
-  //               direction: Axis.horizontal,
-  //               children: [
-  //                 Container(
-  //                   alignment: Alignment.center,
-  //                   padding: const EdgeInsets.all(10),
-  //                   decoration: BoxDecoration(
-  //                       borderRadius: defaultBoxRadius,
-  //                       boxShadow: defaultBoxShadow,
-  //                       border: Border.all(color: primaryColor, width: 2),
-  //                       color: Colors.white),
-  //                   child: Column(
-  //                     children: [
-  //                       Text(
-  //                         "다른 일정",
-  //                         style: mainFont(
-  //                             color: pointColor,
-  //                             fontWeight: FontWeight.w700,
-  //                             fontSize: 13),
-  //                       ),
-  //                       const SizedBox(
-  //                         height: 5,
-  //                       ),
-  //                       Text(
-  //                         place.placeName!,
-  //                         style: mainFont(
-  //                             color: primaryColor,
-  //                             fontWeight: FontWeight.w700,
-  //                             fontSize: 14),
-  //                       ),
-  //                       const SizedBox(
-  //                         height: 8,
-  //                       ),
-  //                       Text(
-  //                         "${printDateTimeHourAndMinuteOnly(place.startsAt!)} ~ ${printDateTimeHourAndMinuteOnly(place.endsAt!)}",
-  //                         style: mainFont(
-  //                             fontWeight: FontWeight.w500,
-  //                             color: primaryColor,
-  //                             fontSize: 12,
-  //                             letterSpacing: 1),
-  //                       ),
-  //                       Text(
-  //                         printDurationKor(place.duration),
-  //                         style: mainFont(
-  //                             fontWeight: FontWeight.w500,
-  //                             color: primaryColor,
-  //                             fontSize: 12,
-  //                             letterSpacing: 1),
-  //                       ),
-  //                     ],
-  //                   ),
-  //                 ),
-  //               ]),
-  //           CustomPaint(
-  //             size: const Size(20, 15),
-  //             painter: DrawTriangleShape(),
-  //           )
-  //         ],
-  //       ),
-  //     );
-  //   }
-  // }
 
   _onTap() {
     return showDialog(
@@ -382,7 +162,6 @@ Future<Marker> markerForPlace(
         snippet:
             "${printDateTimeHourAndMinuteOnly(place.startsAt!)} ~ ${printDateTimeHourAndMinuteOnly(place.endsAt!)}",
         onTap: _onTap),
-    onTap: _onTap,
   );
 }
 
@@ -391,27 +170,6 @@ Future<Marker> markerForCreatedRoute({
   required int order,
   required Place place,
 }) async {
-  // Widget _widget() {
-  //   Color placeColor = placeColorByPlaceType(place.placeType);
-  //   return Container(
-  //     alignment: Alignment.center,
-  //     //padding: const EdgeInsets.all(10),
-  //     decoration: BoxDecoration(
-  //         borderRadius: defaultBoxRadius,
-  //         boxShadow: defaultBoxShadow,
-  //         border: Border.all(color: placeColor, width: 7),
-  //         color: Colors.white),
-  //     width: 35,
-  //     height: 35,
-  //     child: FittedBox(
-  //       child: Text(
-  //         order.toString(),
-  //         style: mainFont(color: placeColor, fontWeight: FontWeight.w900),
-  //       ),
-  //     ),
-  //   );
-  // }
-
   return Marker(
       anchor: const Offset(0.5, 0.5),
       markerId: MarkerId(place.placeId!),
